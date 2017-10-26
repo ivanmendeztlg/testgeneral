d5b7e87402d2a5aead125aaafa2bc64b24b9b61c
Ivan Mendez <ivan.mendez.tlg@gmail.com>
2017-10-26 10:24:13 +0200

    Commit desde Jenkins

--- Test Suites/SuiteLogin.ts
+++ Test Suites/SuiteLogin.ts
@@ -4,6 +4,7 @@
    <name>SuiteLogin</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2017-10-26T10:19:54</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
