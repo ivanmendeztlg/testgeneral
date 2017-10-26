a36465f1251c8fb0cc2e24d7095f505d255cc158
Ivan Mendez <ivan.mendez.tlg@gmail.com>
2017-10-26 10:53:55 +0200

    Commit desde Jenkins

--- Test Suites/SuiteLogin.ts
+++ Test Suites/SuiteLogin.ts
@@ -4,7 +4,6 @@
    <name>SuiteLogin</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-10-26T10:40:23</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -20,19 +19,19 @@
    <testCaseLink>
       <guid>3288d324-0148-421f-83d7-f5af46092814</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/login</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>34021cc7-2f0f-46ef-a209-11e073ff0a3b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/logout</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>eb5e1001-386a-4e32-aa96-c3e16097fb50</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Delete</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
