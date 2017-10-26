d2883f98e4c7e865329d17ee7dcc1e41ee6a7cd7
Ivan Mendez <ivan.mendez.tlg@gmail.com>
2017-10-26 10:13:51 +0200

    segundo commit al proyecto clonado

---
+++ Test Suites/SuiteLogin.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>91f39c87-d799-4a30-b483-a31a5afca9ae</testSuiteGuid>
+   <testCaseLink>
+      <guid>97b4d9a2-197d-4ea2-8436-0e324a3a26ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Crear User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3288d324-0148-421f-83d7-f5af46092814</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34021cc7-2f0f-46ef-a209-11e073ff0a3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/logout</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eb5e1001-386a-4e32-aa96-c3e16097fb50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
